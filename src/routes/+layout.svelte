6a5d049f1b949ce2b8c8e43549fab0205862038a
Rizky Dana <rizkydana14@gmail.com>
2026-05-18 13:20:46 +0800

    feat(routes): add root layout and error handling with i18n

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,7 @@
 <script lang="ts">
-	import favicon from '$lib/assets/favicon.svg';
+	import "$lib/i18n";
 
 	let { children } = $props();
 </script>
-
-<svelte:head>
-	<link rel="icon" href={favicon} />
-</svelte:head>
 
 {@render children()}
