029bd352544288823e5d76ba806961cbe82c5e65
Rizky Dana <rizkydana14@gmail.com>
2026-05-18 12:55:21 +0800

    feat(i18n): add Indonesian language translations and i18n setup

---
+++ src/lib/i18n/index.ts
@@ -0,0 +1,8 @@
+import { getLocaleFromNavigator, init, register } from "svelte-i18n";
+
+register("id", () => import("./id.json"));
+
+init({
+	fallbackLocale: "id",
+	initialLocale: getLocaleFromNavigator(),
+});
