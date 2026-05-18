c56fea34741e09c2cc2223c861c153ef638f5959
Rizky Dana <rizkydana14@gmail.com>
2026-05-18 13:23:42 +0800

    chore(config): update build and type configuration with UnoCSS and test setup

--- svelte.config.js
+++ svelte.config.js
@@ -1,20 +1,33 @@
-import adapter from '@sveltejs/adapter-cloudflare';
+import adapter from "@sveltejs/adapter-cloudflare";
+import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
+import UnoCSS from "@unocss/svelte-scoped/preprocess";
+
+const newLocal = /[/\\]/;
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	compilerOptions: {
 		// Force runes mode for the project, except for libraries. Can be removed in svelte 6.
-		runes: ({ filename }) => filename.split(/[/\\]/).includes('node_modules') ? undefined : true
+		runes: ({ filename }) =>
+			filename.split(newLocal).includes("node_modules") ? undefined : true,
 	},
 	kit: {
 		adapter: adapter(),
 		typescript: {
 			config: (config) => ({
 				...config,
-				include: [...config.include, '../drizzle.config.ts']
-			})
-		}
-	}
+				include: [
+					...config.include,
+					"../drizzle.config.ts",
+					"./drizzle.local.config.ts",
+				],
+			}),
+		},
+	},
+	preprocess: [
+		vitePreprocess(),
+		UnoCSS({ configOrPath: "./uno.config.ts", exclude: ["./node_modules"] }),
+	],
 };
 
 export default config;
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,27 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite-plus';
+import path from "node:path";
+import { sveltekit } from "@sveltejs/kit/vite";
+import UnoCSS from "unocss/vite";
+import { defineConfig } from "vite-plus";
 
 export default defineConfig({
-  staged: {
-    "*": "vp check --fix"
-  },
-  fmt: {},
-  lint: {"options":{"typeAware":true,"typeCheck":true}},
-  plugins: [sveltekit()]
+	staged: {
+		"*": "vp check --fix",
+	},
+	fmt: {},
+	lint: { options: { typeAware: true, typeCheck: true } },
+	plugins: [UnoCSS(), sveltekit()],
+	server: {
+		host: true,
+		port: 5432,
+	},
+	resolve: {
+		alias: {
+			$test: path.resolve("./src/test"),
+		},
+	},
+	test: {
+		environmentMatchGlobs: [["src/lib/components/**", "happy-dom"]],
+		globals: true,
+		setupFiles: ["./src/test/setup.ts"],
+	},
 });
