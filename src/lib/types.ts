bedf3ed126a3cda659b7f99afb4ffd3faba96405
Rizky Dana <rizkydana14@gmail.com>
2026-05-18 12:57:51 +0800

    feat(db): add Kysely type definitions and application types

---
+++ src/lib/types.ts
@@ -0,0 +1,21 @@
+export interface Item {
+	checked: boolean;
+	createdAt: number;
+	id: string;
+	listId: string;
+	name: string;
+	updatedAt: number;
+}
+
+export interface List {
+	createdAt: number;
+	id: string;
+	name: string;
+	updatedAt: number;
+	userId?: string | null;
+}
+
+export interface ListWithStats extends List {
+	checkedCount: number;
+	totalCount: number;
+}
