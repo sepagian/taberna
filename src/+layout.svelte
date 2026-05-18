e5c0e673fae3cc5087e4945ab4655bb5aee7a89f
Rizky Dana <rizkydana14@gmail.com>
2026-05-18 14:54:11 +0800

    refactor(layout): consolidate root layout into routes directory

--- src/+layout.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-	import { ModeWatcher } from "mode-watcher";
-
-	let { children } = $props();
-</script>
-
-<ModeWatcher />
-{@render children?.()}
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,11 @@
 <script lang="ts">
 	import "$lib/i18n";
+	import "@unocss/reset/tailwind-v4.css";
+	import "uno.css";
+	import { ModeWatcher } from "mode-watcher";
 
 	let { children } = $props();
 </script>
+<ModeWatcher />
 
 {@render children()}
