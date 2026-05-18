66a6bed819c198b673622deaf22fd089ccd18647
Rizky Dana <rizkydana14@gmail.com>
2026-05-18 14:38:55 +0800

    feat(routes): add root layout and landing page

---
+++ src/routes/+layout.ts
@@ -0,0 +1,6 @@
+import { waitLocale } from "svelte-i18n";
+import type { LayoutLoad } from "./$types";
+
+export const load: LayoutLoad = async () => {
+	await waitLocale();
+};
---
+++ src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import { MotionButton } from "@humanspeak/svelte-motion";
+	import { goto } from "$app/navigation";
+</script>
+
+<MotionButton
+	type="button"
+	class="flex w-full justify-center h-12 rounded-md items-center font-semibold bg-primary text-primary-foreground"
+	onclick={() => goto("/app")}
+>
+	Go to App
+</MotionButton>
