e52918900068fe0e21ecddcf0ec763368fa4509a
Rizky Dana <rizkydana14@gmail.com>
2026-05-18 14:39:24 +0800

    feat(auth): refactor auth configuration for enhanced security and session management

--- src/lib/server/auth.ts
+++ src/lib/server/auth.ts
@@ -5,28 +5,46 @@ import { getRequestEvent } from "$app/server";
 import { env } from "$env/dynamic/private";
 import { getKysely } from "$lib/server/db";
 
-const authConfig = {
-	baseURL: env.ORIGIN,
-	secret: env.BETTER_AUTH_SECRET,
-	account: {
-		accountLinking: {
-			trustedProviders: ["google"],
+const getAuthConfig = () =>
+	({
+		baseURL: env.ORIGIN,
+		secret: env.BETTER_AUTH_SECRET,
+		account: {
+			accountLinking: {
+				trustedProviders: ["google"],
+			},
 		},
-	},
-	emailAndPassword: { enabled: false },
-	socialProviders: {
-		google: {
-			clientId: env.GOOGLE_CLIENT_ID as string,
-			clientSecret: env.GOOGLE_CLIENT_SECRET as string,
+		emailAndPassword: { enabled: false },
+		socialProviders: {
+			google: {
+				clientId: env.GOOGLE_CLIENT_ID as string,
+				clientSecret: env.GOOGLE_CLIENT_SECRET as string,
+			},
 		},
-	},
-	plugins: [
-		sveltekitCookies(getRequestEvent), // make sure this is the last plugin in the array
-	],
-} satisfies Omit<Parameters<typeof betterAuth>[0], "database">;
+		session: {
+			cookieCache: {
+				enabled: true,
+				maxAge: 60,
+			},
+		},
+		plugins: [
+			sveltekitCookies(getRequestEvent),
+		],
+		advanced: {
+			defaultCookieAttributes: {
+				sameSite: "none" as const,
+				secure: true,
+				httpOnly: true,
+			},
+			crossSubDomainCookies: {
+				enabled: true,
+				domain: "taberna.rizky.workers.dev",
+			},
+		},
+	}) satisfies Omit<Parameters<typeof betterAuth>[0], "database">;
 
 export const createAuth = (d1: D1Database | null) =>
 	betterAuth({
-		...authConfig,
+		...getAuthConfig(),
 		database: kyselyAdapter(getKysely(d1), { type: "sqlite" }),
 	});
